adf7d1019b74d8587d58821db733e8f1bf2164f7
Boomshakalak <soarer@vip.qq.com>
2017-05-15 22:29:05 -0500

    #something

---
+++ matrixElementsSum.cpp
@@ -0,0 +1,10 @@
+int matrixElementsSum(std::vector<std::vector<int>> matrix) {
+    int res = 0;
+    for (int i = 0 ; i <matrix[0].size() ; ++i){
+        for (int j = 0 ; j<matrix.size(); ++j){
+            res+= matrix[j][i];
+            if (matrix[j][i] == 0 ) break;
+        }
+    }
+    return res;
+}
